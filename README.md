694d14f1e0b41fb97dc0852ab8d7b5c2c6e9ef28
Angelusualle <angelusualle@gmail.com>
2018-05-15 10:02:28 -0400

    feat: Timer feature added

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # Memory Game Project
 
-## Table of Contents
+## About
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+This is the submission to the Memory Game project as part of Udacity's Front End Nanodegree Program.
 
-## Instructions
+It is a single page application created as a game for matching cards based on memory. There is a number of moves counter, timer, and star rating system.
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+The object of the game is to match all cards as fast as possible.
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+No contributing necessary.
--- js/app.js
+++ js/app.js
@@ -1,25 +1,74 @@
-/*
- * Create a list that holds all of your cards
- */
+//Initializing all global variables
 'use strict';
-let startTime = new Date();
+let startTime;
 let endTime;
 let cards = [];
 let activeCard;
 let matches = 0;
 let moves = 0;
 let stars = 3;
+let timer;
 
+//Setting up event listeners
+document.querySelector('.close').addEventListener('click', () => document.querySelector('#winner').style.display = 'none');
+document.querySelector('#btnPlayAgain').addEventListener('click', () => window.location.reload());
+document.querySelector('.restart').addEventListener('click', () => window.location.reload());
+
+//Click event handler will perform all logic here including keeping track of score
+document.querySelector('.deck').addEventListener('click', (clickCardEvent) => {
+    if (clickCardEvent.target.nodeName == 'LI' && !clickCardEvent.target.classList.contains('open')) {
+        if (startTime == undefined) {
+            startGame();
+        }
+        openCard(clickCardEvent);
+        if (!activeCard) {
+            activeCard = clickCardEvent.target;
+        }
+        else {
+            ++moves;
+            if (moves % 8 == 0 && stars > 0) {
+                --stars;
+                const star = document.querySelector('.fa-star');
+                star.classList.remove('fa-star');
+                star.classList.add('fa-star-o');
+            }
+            document.querySelector('.score-panel span').innerText = moves;
+            if (clickCardEvent.target.children[0].className == activeCard.children[0].className) {
+                clickCardEvent.target.classList.add('match');
+                activeCard.classList.add('match');
+                ++matches;
+                if (matches == 8) {
+                    winner();
+                }
+                activeCard = undefined;
+            }
+            else {
+                closeCard(activeCard);
+                closeCard(clickCardEvent.target);
+                let interval = setInterval(() => {
+                    const listOfNotMatchedCards = document.querySelectorAll('.notMatch');
+                    for (let i = 0; i < listOfNotMatchedCards.length; ++i) {
+                        markAsUnMatched(listOfNotMatchedCards, i);
+                    }
+                    clearInterval(interval);
+                }, 500);
+                activeCard = undefined;
+            }
+        }
+    }
+});
+
+//Initializing deck
 createDeck();
 Reset();
 document.querySelector('.score-panel span').innerText = moves;
-
 
 function Reset() {
     shuffle();
     updateHTML();
 }
 
+//Updates deck HTML using global variables
 function updateHTML() {
     const HTMLDeck = document.querySelector('.deck');
     HTMLDeck.innerHTML = '';
@@ -28,6 +77,7 @@ function updateHTML() {
     HTMLDeck.appendChild(docFrag);
 }
 
+//Creates deck saves in global variable
 function createDeck() {
     cards = [];
     document.querySelector('.deck').innerHTML = '';
@@ -43,7 +93,7 @@ function createDeck() {
     }
 }
 
-
+//Returns HTML card given string type of card
 function createCard(type) {
     const card = document.createElement('li');
     card.classList.add('card');
@@ -52,11 +102,6 @@ function createCard(type) {
     card.appendChild(innerTag);
     return card;
 }
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976 modified
 function shuffle() {
@@ -71,81 +116,36 @@ function shuffle() {
     }
 }
 
+//Given a list of cards will flip over to not be seen after unmatched animation
+function markAsUnMatched(listOfNotMatchedCards, i) {
+    listOfNotMatchedCards[i].classList.remove('open');
+    listOfNotMatchedCards[i].classList.remove('show');
+    listOfNotMatchedCards[i].classList.remove('notMatch');
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-document.querySelector('.deck').addEventListener('click', (clickCardEvent) => {
-    if (clickCardEvent.target.nodeName == 'LI' && !clickCardEvent.target.classList.contains('open')) {
-       
-        open(clickCardEvent);
-        if (!activeCard) {
-            activeCard = clickCardEvent.target;
-        }
-        else {
-            ++moves;
-            if (moves % 8 == 0 && stars > 0) {
-                --stars;
+//Starts the game by initializing startTime and setting timer for updating this span.
+function startGame() {
+    startTime = new Date();
+    timer = setInterval(() => document.querySelector('.timer > span').innerText = Math.floor(((new Date()) - startTime) / (1000)), 1000);
+}
 
-            }
-            document.querySelector('.score-panel span').innerText = moves;
-            if (clickCardEvent.target.children[0].className == activeCard.children[0].className) {
-                clickCardEvent.target.classList.add('match');
-                activeCard.classList.add('match');
-                ++matches;
-                if (matches == 8) {
-                    winner();
-                }
-                activeCard = undefined;
-            }
-            else {
-                close(activeCard);
-                close(clickCardEvent.target);
-                let interval = setInterval(() => {
-                    const listOfNotMatchedCards = document.querySelectorAll('.notMatch');
-                    for (let i = 0; i < listOfNotMatchedCards.length; ++i) {
-                        listOfNotMatchedCards[i].classList.remove('open');
-                        listOfNotMatchedCards[i].classList.remove('show');
-                        listOfNotMatchedCards[i].classList.remove('notMatch');
-                    }
-                    clearInterval(interval);
-                }, 500);
-                activeCard = undefined;
-            }
-        }
-
-    }
-
-});
-
+//Handles winnning the game by initiating pop up also clears timer.
 function winner() {
     endTime = new Date();
     document.querySelector('.popup-content p').innerText = document.querySelector('.popup-content p').innerText.replace('x', moves);
     document.querySelector('.popup-content p').innerText = document.querySelector('.popup-content p').innerText.replace('Y', stars);
     document.querySelector('.popup-content p').innerText = document.querySelector('.popup-content p').innerText.replace('Z', Math.floor((endTime - startTime) / (1000)));
     document.querySelector('#winner').style.display = 'block';
+    clearInterval(timer);
 }
 
-function open(clickCardEvent) {
+//Opens card by adding necessary CSS classes
+function openCard(clickCardEvent) {
     clickCardEvent.target.classList.add('open');
     clickCardEvent.target.classList.add('show');
 }
 
-function close(card) {
+//Closes card by adding necessary CSS class for animation.
+function closeCard(card) {
     card.classList.add('notMatch');
 }
-
-document.querySelector('.close').addEventListener('click', () => document.querySelector('#winner').style.display = 'none');
-document.querySelector('#btnPlayAgain').addEventListener('click', () => window.location.reload());
-document.querySelector('.restart').addEventListener('click', () => window.location.reload());
-
-//testing start
-document.querySelector('#test').addEventListener('click', () => winner());
-//testing end
